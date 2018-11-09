b0f0274493b5a3a18ec57abe4474c90ae470539e
snotpig <snotpig@users.noreply.github.com>
2018-11-09 22:57:19 +0000

    First Commit

---
+++ USBDoorbell/USBDoorbell/Form1.cs
@@ -0,0 +1,247 @@
+﻿using BuzzWin;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace USBDoorbell
+{
+    public partial class Form1 : UsbAwareForm
+    {
+        private const int NUMSOUNDS = 5;
+
+        private NotifyIcon  trayIcon;
+        private ContextMenu trayMenu;
+        private List<MenuItem> soundItems;
+        private Icon connectedIcon;
+        private Icon disconnectedIcon;
+        private Icon mutedIcon;
+  
+        private MyDevice device;
+        private System.Media.SoundPlayer player;
+        private System.Timers.Timer timerBell = new System.Timers.Timer();
+        private bool isPlaying = false;
+        private bool isConnected = false;
+        private bool isMuted = false;
+        private string path;
+
+        public Form1()
+        {
+            InitializeComponent();
+            path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            connectedIcon = new Icon(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("USBDoorbell.Resources.goldBell.ico"));
+            disconnectedIcon = new Icon(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("USBDoorbell.Resources.greyBell.ico"));
+            mutedIcon = new Icon(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("USBDoorbell.Resources.muteBell.ico"));
+
+            soundItems = new List<MenuItem>{
+                new MenuItem("Ding Dong", OnDingDong),
+                new MenuItem("Ding Dong Ding Dong", OnDingDong2),
+                new MenuItem("Buzzer", OnBuzzer),
+                new MenuItem("Ring Ring", OnRingRing),
+                new MenuItem("Mute", OnMute)
+            };
+
+            trayMenu = new ContextMenu();
+            trayMenu.MenuItems.Add("Test Bell", OnTestBell);
+            trayMenu.MenuItems.Add("Choose Bell Sound", soundItems.ToArray());
+            trayMenu.MenuItems.Add("Open Log", OnLog);
+            trayMenu.MenuItems.Add("Exit", OnExit);
+ 
+            trayIcon = new NotifyIcon();
+            trayIcon.Text = "Doorbell Monitor";
+            trayIcon.Icon = disconnectedIcon;
+            trayIcon.ContextMenu = trayMenu;
+            trayIcon.Visible     = true;
+
+            player = new System.Media.SoundPlayer(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(@"USBDoorbell.Media.DingDong2.wav"));
+            soundItems[1].Checked = true;
+            timerBell.Interval = 2000;
+            timerBell.Elapsed += new System.Timers.ElapsedEventHandler(bellTimer_Tick);
+            isConnected = Connect();
+            ShowStatus();
+        }
+
+        private static void SetLabelText(Label label, string text)
+        {
+            label.Text = text;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false; // Hide form window.
+            ShowInTaskbar = false; // Remove from taskbar.
+            base.OnLoad(e);
+        }
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            trayIcon.Dispose();
+            Application.Exit();
+        }
+
+        private bool Connect()
+        {
+            device = MyDevice.FindDevice();
+            if (device != null)
+            {
+                device.OnButtonChanged += button_Press;
+                return true;
+            }
+            return false;
+        }
+
+        private void ShowStatus()
+        {
+            if (isConnected)
+            {
+                label1.Visible = false;
+                if (!isMuted) trayIcon.Icon = connectedIcon;
+                else trayIcon.Icon = mutedIcon;
+            }
+            else
+            {
+                label1.Visible = true;
+                trayIcon.Icon = disconnectedIcon;
+            }
+        }
+
+        private void button_Press(object sender, EventArgs e)
+        {
+            RingBell();
+        }
+
+        private void RingBell()
+        {
+            if (!isPlaying)
+            {
+                isPlaying = true;
+                trayIcon.ShowBalloonTip(3600000, "Door!!!", "There's somebody at the door!", ToolTipIcon.Info);
+                player.Play();
+                WriteLog();
+                timerBell.Start();
+            }
+        }
+
+        private void WriteLog()
+        {
+            try
+            {
+                Directory.CreateDirectory(path + "\\Doorbell");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            using(TextWriter tw = new StreamWriter(path + "\\Doorbell\\log.txt", true))
+            {
+                tw.WriteLine(DateTime.Now);
+            }
+                
+        }
+
+
+        protected override void OnDeviceArrived(EventArgs args)
+        {
+            base.OnDeviceArrived(args);
+            if (!isConnected) isConnected = Connect();
+            ShowStatus();
+        }
+
+        protected override void OnDeviceRemoved(EventArgs args)
+        {
+            base.OnDeviceRemoved(args);
+            isConnected = MyDevice.CheckPresent();
+            ShowStatus();
+        }
+
+        private void UncheckAllSoundItems()
+        {
+            foreach( var item in soundItems)
+            {
+                item.Checked = false;
+            }
+        }
+
+        #region Sound Choice Events
+
+        protected void OnDingDong(object sender, EventArgs e)
+        {
+            player = new System.Media.SoundPlayer(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(@"USBDoorbell.Media.DingDong.wav"));
+            UncheckAllSoundItems();
+            soundItems[0].Checked = true;
+            isMuted = false;
+            ShowStatus();
+        }
+
+        protected void OnDingDong2(object sender, EventArgs e)
+        {
+            player = new System.Media.SoundPlayer(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(@"USBDoorbell.Media.DingDong2.wav"));
+            UncheckAllSoundItems();
+            soundItems[1].Checked = true;
+            isMuted = false;
+            ShowStatus();
+        }
+
+        protected void OnBuzzer(object sender, EventArgs e)
+        {
+            player = new System.Media.SoundPlayer(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(@"USBDoorbell.Media.Buzzer.wav"));
+            UncheckAllSoundItems();
+            soundItems[2].Checked = true;
+            isMuted = false;
+            ShowStatus();
+        }
+
+        protected void OnRingRing(object sender, EventArgs e)
+        {
+            player = new System.Media.SoundPlayer(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(@"USBDoorbell.Media.RingRing.wav"));
+            UncheckAllSoundItems();
+            soundItems[3].Checked = true;
+            isMuted = false;
+            ShowStatus();
+        }
+
+        protected void OnMute(object sender, EventArgs e)
+        {
+            player = new System.Media.SoundPlayer(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(@"Doorbell.Media.Silence.wav"));
+            UncheckAllSoundItems();
+            soundItems[4].Checked = true;
+            isMuted = true;
+            ShowStatus();
+        }
+
+        #endregion
+
+        protected void OnLog(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(path + "\\Doorbell"))
+            {
+                Directory.CreateDirectory(path + "\\Doorbell");               
+            }
+            if (!File.Exists(path + "\\Doorbell\\log.txt"))
+            {
+                File.Create(path + "\\Doorbell\\log.txt");
+            }
+            else Process.Start(path + "\\Doorbell\\log.txt");
+        }
+
+        protected void OnTestBell(object sender, EventArgs e)
+        {
+            player.Play();
+        }
+ 
+        void bellTimer_Tick(object sender, EventArgs e)
+        {
+            timerBell.Stop();
+            isPlaying = false;
+        }
+
+        private void OnTestButton(object sender, EventArgs e)
+        {
+            WriteLog();
+        }
+
+    }
+}
---
+++ USBDoorbell/USBDoorbell/MyDevice.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BuzzWin;
+
+namespace BuzzWin
+{
+    public class ButtonStates
+    {
+        public bool Inp1;
+        public bool Inp2;
+        public bool Inp3;
+        public bool Inp4;
+        public bool Inp5;
+    }
+
+    public class ButtonChangedEventArgs : EventArgs
+    {
+        /// <summary>Current states of the buttons</summary>
+        public readonly ButtonStates Buttons;
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="buttons">State of the buttons</param>
+        public ButtonChangedEventArgs(ButtonStates buttons)
+        {
+            Buttons = buttons;
+        }
+    }
+
+    /// <summary>
+    /// Delegate for button event
+    /// </summary>
+    public delegate void ButtonChangedEventHandler(object sender, ButtonChangedEventArgs args);
+
+
+    class MyDevice : HIDDevice
+    {
+        private const int VID = 0x04d8; //0x10cf;// ***
+        private const int PID = 0xCC77; //0x5500;// ***
+
+        private Byte mButtons;
+        public event ButtonChangedEventHandler OnButtonChanged;
+
+        public override InputReport CreateInputReport()
+        {
+            return new MyInputReport(this);
+        }
+
+        public static bool CheckPresent()
+        {
+            return CheckPresent(VID, PID);
+        }
+
+        public static MyDevice FindDevice()
+        {
+            return (MyDevice)FindDevice(VID, PID, typeof(MyDevice));
+        }
+
+        protected override void HandleDataReceived(InputReport oInRep)
+        {
+            if(mButtons != oInRep.Buffer[1])
+            {
+                mButtons = oInRep.Buffer[1];
+                if (OnButtonChanged != null)
+                {
+                    if(mButtons == 29)
+                    {
+                        // Fire the event handler if assigned
+                        MyInputReport myIn = (MyInputReport)oInRep;
+                        OnButtonChanged(this, new ButtonChangedEventArgs(myIn.Buttons));
+                    }
+                }
+            }
+        }
+
+
+        public void SetOutput(bool out1, bool out2, bool out3, bool out4, bool out5, bool out6, bool out7, bool out8)
+        {
+            MyOutputReport oRep = new MyOutputReport(this);	// create output report
+            oRep.SetLightStates(out1, out2, out3, out4, out5, out6, out7, out8);	// set the lights states
+            try
+            {
+                Write(oRep); // write the output report
+            }
+            catch (HIDDeviceException ex)
+            {
+                throw new Exception("Disconnected!");
+                // Disconnected?
+            }
+        }
+
+        public class MyInputReport : InputReport
+        {
+            public ButtonStates Buttons = new ButtonStates();
+
+            public MyInputReport(HIDDevice device) : base(device) {}
+
+            public override void ProcessData()
+            {
+                byte[] arrData = Buffer;
+                Buttons.Inp1 = ((arrData[1] & 0x10) != 0);
+                Buttons.Inp2 = ((arrData[1] & 0x20) != 0);
+                Buttons.Inp3 = ((arrData[1] & 0x01) != 0);
+                Buttons.Inp4 = ((arrData[1] & 0x40) != 0);
+                Buttons.Inp5 = ((arrData[1] & 0x80) != 0);
+            }
+        }
+
+        public class MyOutputReport : OutputReport
+        {
+		    public MyOutputReport(HIDDevice device) : base(device) {}
+
+            public void SetLightStates(bool out1, bool out2, bool out3, bool out4, bool out5, bool out6, bool out7, bool out8)
+            {
+                byte[] arrBuff = Buffer;
+
+                arrBuff[0] = 0;// (byte)(out1 ? 0xff : 0);
+                arrBuff[2] = (byte)(out2 ? 0xff : 0);
+                arrBuff[3] = (byte)(out3 ? 0xff : 0);
+                arrBuff[4] = (byte)(out4 ? 0xff : 0);
+                arrBuff[5] = (byte)(out5 ? 0xff : 0);
+                arrBuff[6] = (byte)(out6 ? 0xff : 0);
+                arrBuff[7] = (byte)(out7 ? 0xff : 0);
+                arrBuff[8] = (byte)(out8 ? 0xff : 0);
+            }
+        }
+
+    }
+}
